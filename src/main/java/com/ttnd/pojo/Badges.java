9bb8679cb11c4cad3cb4cb3a896cda9fd2ebb827
Akshay <akkiriyal@gmail.com>
2016-07-07 17:21:02 +0530

    Reap Project

---
+++ src/main/java/com/ttnd/pojo/Badges.java
@@ -0,0 +1,30 @@
+package com.ttnd.pojo;
+
+import javax.persistence.Id;
+
+public class Badges {
+	@Id
+ private Integer badge_id;
+	 private Integer badge_type;
+	 private Integer badge_weight;
+	public Integer getBadge_id() {
+		return badge_id;
+	}
+	public void setBadge_id(Integer badge_id) {
+		this.badge_id = badge_id;
+	}
+	public Integer getBadge_weight() {
+		return badge_weight;
+	}
+	public void setBadge_weight(Integer badge_weight) {
+		this.badge_weight = badge_weight;
+	}
+	public Integer getBadge_type() {
+		return badge_type;
+	}
+	public void setBadge_type(Integer badge_type) {
+		this.badge_type = badge_type;
+	}
+
+
+}
---
+++ src/main/java/com/ttnd/pojo/Department.java
@@ -0,0 +1,29 @@
+package com.ttnd.pojo;
+
+import javax.persistence.Id;
+
+public class Department {
+	@Id
+	private Integer dept_id;
+	private String practice;
+	private String service_line;
+	public String getPractice() {
+		return practice;
+	}
+	public void setPractice(String practice) {
+		this.practice = practice;
+	}
+	public Integer getDept_id() {
+		return dept_id;
+	}
+	public void setDept_id(Integer dept_id) {
+		this.dept_id = dept_id;
+	}
+	public String getService_line() {
+		return service_line;
+	}
+	public void setService_line(String service_line) {
+		this.service_line = service_line;
+	}
+
+}
